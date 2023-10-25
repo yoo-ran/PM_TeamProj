034a811cecc46add31a049aa3801b60d540bf57e
YooranKim <yuranm@naver.com>
2023-10-25 14:45:07 -0700

    checkoutpage

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const checkout = () => {
-    console.log("tt");
     document.querySelector(".checkout").style.display = "block"
     window.scrollTo(0,0);
-    console.log(document.documentElement.scrollHeight);
     document.querySelector(".bg").style.height = `${document.documentElement.scrollHeight}px`
+}
+
+const disappear = () => {
+    document.querySelector(".checkout").style.display = "none"
+    document.querySelector(".bg").style.display = "none"
+
 }
